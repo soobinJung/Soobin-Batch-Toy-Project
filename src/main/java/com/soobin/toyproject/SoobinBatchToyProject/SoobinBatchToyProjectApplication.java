658dd95818303c081092e4e86bb936d848f91a7d
soobinjung <sbjung@realsn.com>
2024-01-27 11:22:40 +0900

    init

---
+++ src/main/java/com/soobin/toyproject/SoobinBatchToyProject/SoobinBatchToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.soobin.toyproject.SoobinBatchToyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoobinBatchToyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoobinBatchToyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soobin/toyproject/SoobinBatchToyProject/SoobinBatchToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soobin.toyproject.SoobinBatchToyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoobinBatchToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
